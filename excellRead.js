c2b13b5ee640bb69756754b24ab421c9bc419126
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-10 21:57:24 +0300

    workbook read file

--- excellRead.js
+++ excellRead.js
@@ -1,4 +1,7 @@
 //installing the npm install exceljs 
 const ExcelJS = require('exceljs');
-
-
+// we create workbook object 
+const workbook = new ExcelJS.Workbook();
+workbook.xlsx.readFile('SummitGR.xlsx').then(() => {
+    
+});
