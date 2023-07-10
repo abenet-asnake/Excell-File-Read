c6c7a803fef636edd745838966fe6b7ce8966668
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-10 22:03:35 +0300

    accessing the worksheet

--- excellRead.js
+++ excellRead.js
@@ -3,5 +3,6 @@ const ExcelJS = require('exceljs');
 // we create workbook object 
 const workbook = new ExcelJS.Workbook();
 workbook.xlsx.readFile('SummitGR.xlsx').then(() => {
-    
+    // Access the worksheets in the workbook
+    const worksheet = workbook.getWorksheet('summit');
 });
