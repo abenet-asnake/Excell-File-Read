9de95a1e20f95f4060bf549226c6e9a1da0b89dc
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-16 00:18:49 +0300

    it reads sheets on the temp and data array

--- excellRead.js
+++ excellRead.js
@@ -7,3 +7,14 @@ const readingExcel = excelReader.readFile('SUmmitGR.xlsx');
 let sheetData= [];
 
 const readAllSheets=readingExcel.SheetNames;
+for(let i = 0; i < readAllSheets.length; i++)
+{
+   const temp = excelReader.utils.sheet_to_json(
+        readingExcel.Sheets[readingExcel.SheetNames[i]])
+   temp.forEach((res) => {
+      sheetData.push(res)
+   })
+}
+  
+// Printing data
+console.log(sheetData)
