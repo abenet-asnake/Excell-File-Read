0bb4b0441feea835330aba3336e4a2cb32fdde1b
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-15 23:40:39 +0300

    creating reading file and data

--- excellRead.js
+++ excellRead.js
@@ -1,3 +1,9 @@
 // installing npm instal xlsx 
 const excelReader = require('xlsx');
 
+// reading the file SUmmitGR.xlsx
+const readingExcel = excelReader.readFile('SUmmitGR.xlsx');
+
+let sheetData= [];
+
+
