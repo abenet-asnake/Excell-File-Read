f3ed7646ecb15a5d1c024cf7b6c98b195b1a5576
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-15 15:02:13 +0300

    each rom is not define

--- excellRead.js
+++ excellRead.js
@@ -6,12 +6,11 @@ workbook.xlsx.readFile('SummitGR.xlsx').then(() => {
     // Access the worksheets in the workbook
     const worksheet = workbook.getWorksheet('summit');
 
-    //Iterate through the rows in the worksheet
-    worksheet.eachRow((row,rowNumber) => {
-        // Iterate through the cells in each row
-        row.eachCell((cell,colNumber) => {
-// Access cell values
-console.log('Cell ${cell.address}: ${cell.value}');
+    worksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+        console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
         });
-    });
-});
+    })
+
+.catch((error) => {
+    console.log('Error reading Excel file:', error);
+  });
