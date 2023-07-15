7121a6e6ef98e753a711d0143e0861ac68e6e4fc
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-15 15:05:33 +0300

    installing xlsx

--- excellRead.js
+++ excellRead.js
@@ -1,17 +1,3 @@
-//installing the npm install exceljs 
-const ExcelJS = require('exceljs');
-// we create workbook object 
-const workbook = new ExcelJS.Workbook();
-workbook.xlsx.readFile('SummitGR.xlsx').then(() => {
-    // Access the worksheets in the workbook
-    const worksheet = workbook.getWorksheet('summit');
+// installing npm instal xlsx 
+const excelReader = require('xlsx');
 
-    //Iterate through the rows in the worksheet
-    worksheet.eachRow((row,rowNumber) => {
-        // Iterate through the cells in each row
-        row.eachCell((cell,colNumber) => {
-// Access cell values
-console.log('Cell ${cell.address}: ${cell.value}');
-        });
-    });
-});
